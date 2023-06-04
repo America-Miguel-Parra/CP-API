af9fd899a61af045279d07f23f7bb7770ef3b6aa
America-Miguel-Parra <isc19350307@gmail.com>
2023-06-04 11:05:29 -0600

    Diseño del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import ApiConnection from "./components/ApiConnection"
 function App() {
   return (
     <div>
-      <h1>Verificador de Codigo Postal</h1>
-      <ApiConnection />
+      <h1 className="p-3 bg-dark bg-opacity-75 border border-dark border-start-0 rounded-end fw-bold border border-0 text-white">Verificador de Código Postal</h1>
+
+      <div className="p-3 bg-body-secondary bg-opacity-100 border border-dark border-start-0 rounded-end fw-bold border border-0">
+        <h5>Códigos Postales de México</h5>
+        <h5>Ingresa el código postal de 5 dígitos que quieres buscar y se desplegarán los datos al lugar que pertenece.</h5>
+        <br />
+        <ApiConnection />
+      </div>
+
+      <footer className="p-3 bg-dark bg-opacity-75 border border-dark border-start-0 rounded-end fw-bold border border-0">
+      </footer>
+
     </div>
+
+
   )
 }
 
--- src/components/ApiConnection.jsx
+++ src/components/ApiConnection.jsx
@@ -19,13 +19,21 @@ const ApiConnection = () => {
             {
                 zipCode.map((details) => {
                     return (
-                        <div>
-                            <h1>{details.d_codigo}</h1>
-                            <p>{details.d_asenta}</p>
-                            <p>{details.d_mnpio}</p>
-                            <p>{details.d_estado}</p>
-                            <p>{details.d_ciudad}</p>
+
+                        <div className="container col-md-4 text-center">
+                            <div className="card-body">
+                                <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Ingresa el código postal..." aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                                <button className="btn btn-outline-dark" type="button" id="button-addon2">Buscar</button>
+                                    <h1>{details.d_codigo}</h1>
+                                    <p>{details.d_asenta}</p>
+                                    <p>{details.d_mnpio}</p>
+                                    <p>{details.d_estado}</p>
+                                    <p>{details.d_ciudad}</p>
+                                </div>
+                            </div>
                         </div>
+
                     )
                 })
             }
